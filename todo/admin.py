b61425e208e1435eabf1b511bcb2c20af5634b61
me@ubuntu <ashyrbaew@gmail.com>
2019-11-03 07:11:29 +0600

    new update

--- todo/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import TodoList
-
-admin.site.register(TodoList)
-
--- todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.TodoIndex, name ='IndexPage'),
-    path('<int:item_id>/', views.ChangeStatus, name ='ChangeStatus'),
-]
